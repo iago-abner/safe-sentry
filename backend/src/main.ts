6707fb6c560943f9e7160fd03f69790e6c4e7dab
iago-abner <iagoabner.dev@gmail.com>
2024-06-17 08:56:12 -0300

    feat: adicionando fila para o processamento dos dados

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import { Queue } from 'bullmq'
+import Redis from 'ioredis'
 import db from './pg-connection'
 
 const app = express()
 const PORT = 4242
 
 app.use(express.json())
+
+const connection = new Redis()
 
 app.post('/rastreamento', async (req, res) => {
   const {
@@ -19,6 +23,21 @@ app.post('/rastreamento', async (req, res) => {
     direcao,
     odometro
   } = req.body
+
+  const myQueue = new Queue('myqueue', { connection })
+
+  myQueue.add('rastreamento', {
+    latitude,
+    longitude,
+    velocidade,
+    horario_rastreador,
+    bateria,
+    bateria_veiculo,
+    ignicao,
+    altitude,
+    direcao,
+    odometro
+  })
 
   try {
     const result = await db.pool.query(
