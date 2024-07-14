942cfc9aa50d7e27aefcc70da9a326468727b2c7
iago-abner <iagoabner.dev@gmail.com>
2024-07-14 00:51:13 -0300

    wip

--- frontend/src/app/todo.tsx
+++
@@ -1,7 +0,0 @@
-import { use } from "react";
-
-export function Todo({ todoPromise }: { todoPromise: Promise<any> }) {
-  const todo = use(todoPromise);
-
-  return <pre>{JSON.stringify(todo, null, 2)}</pre>;
-}
